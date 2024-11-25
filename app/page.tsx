be50b9e14201e344ce4599ca227b91865139657d
akhil272 <akhil272@hotmail.com>
2024-11-25 20:39:47 +0530

    updated desktop css

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,27 @@ const ThreeScene = dynamic(() => import("@/components/ThreeScene"), {
 export default function Home() {
   return (
     <div className=" bg-neutral-900">
-      <div className="h-[48svh]">
-        <ThreeScene />
+      <div className="lg:flex lg:items-center">
+        <div className="h-[48svh] lg:w-1/2 lg:h-[100svh]">
+          <ThreeScene />
+        </div>
+        <div className="pt-8 px-8 lg:w-1/2 mx-auto text-white lg:h-[100svh] lg:justify-center lg:flex lg:flex-col ">
+          <h2 className="font-doto font-bold text-4xl lg:text-8xl mt-auto ">
+            Tailored Solutions, Amplified{" "}
+            <span className="text-yellow-500">Success</span>
+          </h2>
+          <div className="flex flex-col mt-auto">
+            <p className="text-neutral-300">
+              Custom software development to augment your business operations
+            </p>
+            <Button className="text-neutral-900 bg-white p-4 my-10 w-2/4">
+              Explore Our Service
+            </Button>
+          </div>
+        </div>
       </div>
+
       <div className="pt-8 px-8 max-w-5xl mx-auto text-white  ">
-        <h2 className="font-doto font-bold text-4xl">
-          Tailored Solutions, Amplified{" "}
-          <span className="text-yellow-500">Success</span>
-        </h2>
-        <p className="text-neutral-300">
-          Custom software development to augment your business operations
-        </p>
         <WhyChooseSection />
         <OurServicesSection />
         <OurTestimonialsSection />
